22cb2aa9e929367e2392049e28d56234e4256493
Thu Nguyen <19522308@gm.uit.edu.vn>
2023-10-26 11:48:58 +0700

    update screenshots

--- client1/src/components/Price.js
+++ client1/src/components/Price.js
@@ -1,27 +1,27 @@
 export const prices = [
-    {
-      _id: 0,
-      name: "$0 to 50",
-      array: [0, 50],
-    },
-    {
-      _id: 1,
-      name: "$50 to 100",
-      array: [51, 100],
-    },
-    {
-      _id: 2,
-      name: "$100 to 150",
-      array: [101, 150],
-    },
-    {
-      _id: 3,
-      name: "$60 to 79",
-      array: [150, 200],
-    },
-    {
-      _id: 4,
-      name: "$201 to more",
-      array: [201, 250],
-    }
-  ];
+  {
+    _id: 0,
+    name: "0 to 15000",
+    array: [0, 15000],
+  },
+  {
+    _id: 1,
+    name: "15000 to 30000",
+    array: [51, 100],
+  },
+  {
+    _id: 2,
+    name: "30000 to 50000",
+    array: [101, 150],
+  },
+  {
+    _id: 3,
+    name: "50000 to 100000",
+    array: [150, 200],
+  },
+  {
+    _id: 4,
+    name: "100000 to more",
+    array: [201, 250],
+  },
+];
--- client1/src/pages/HomePage.js
+++ client1/src/pages/HomePage.js
@@ -1,34 +1,33 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Checkbox, Pagination, Radio} from 'antd';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Checkbox, Pagination, Radio } from "antd";
 
-import ProductCard from '../components/ProductCard';
-import {prices} from '../components/Price';
-import { fetchAllProduct } from '../services/ProductSevice';
-
+import ProductCard from "../components/ProductCard";
+import { prices } from "../components/Price";
+import { fetchAllProduct } from "../services/ProductSevice";
 
 export default function HomePage() {
-  const [products, setProducts] = useState([])
-  const [categories, setCategories] = useState([])
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-  const [checkedCategories, setCheckedCategories] = useState([])
-  const [radioPrice, setRadioPrice] = useState([])
+  const [checkedCategories, setCheckedCategories] = useState([]);
+  const [radioPrice, setRadioPrice] = useState([]);
 
-  const [page, setPage] = useState(1)
-  const [total, setTotal] = useState('')
-  const [limit, setLimit] = useState(8)
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState("");
+  const [limit, setLimit] = useState(8);
 
   const getProductsPerPage = async () => {
     try {
       const res = await fetchAllProduct(page, limit);
-      if(res) {
-        setProducts(res.products)
-        setTotal(res.total)
+      if (res) {
+        setProducts(res.products);
+        setTotal(res.total);
       }
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   const getAllCategories = async () => {
     try {
@@ -44,12 +43,12 @@ export default function HomePage() {
 
   useEffect(() => {
     getProductsPerPage();
-    getAllCategories()
+    getAllCategories();
   }, []);
 
   useEffect(() => {
     getProductsPerPage();
-  }, [page])
+  }, [page]);
 
   useEffect(() => {
     filterProduct();
@@ -57,28 +56,29 @@ export default function HomePage() {
 
   const handleFilterByCategory = (value, id) => {
     let all = [...checkedCategories];
-    if(value) {
-      all.push(id)
+    if (value) {
+      all.push(id);
     } else {
-      all = all.filter(c => c !== id)
+      all = all.filter((c) => c !== id);
     }
-    setCheckedCategories(() => all)
-  }
+    setCheckedCategories(() => all);
+  };
 
   const filterProduct = async (req, res) => {
     try {
-      console.log(checkedCategories, radioPrice)
-      const {data} = await axios.post(process.env.REACT_APP_API + "api/v1/product/product-filters", 
-      {checkedCategories, radioPrice})
-      setProducts(data?.products)
+      console.log(checkedCategories, radioPrice);
+      const { data } = await axios.post(
+        process.env.REACT_APP_API + "api/v1/product/product-filters",
+        { checkedCategories, radioPrice }
+      );
+      setProducts(data?.products);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <>
-      "slide"
       <div className="row">
         <div className="col-md-3">
           <div className="d-flex flex-column mb-3">
@@ -86,7 +86,9 @@ export default function HomePage() {
             {categories?.map((c) => (
               <Checkbox
                 key={c._id}
-                onChange={(e) => handleFilterByCategory(e.target.checked, c._id)}
+                onChange={(e) =>
+                  handleFilterByCategory(e.target.checked, c._id)
+                }
               >
                 {c.name}
               </Checkbox>
@@ -95,39 +97,45 @@ export default function HomePage() {
           <div className="d-flex flex-column mb-3">
             <h3>Price</h3>
             <Radio.Group onChange={(e) => setRadioPrice(e.target.value)}>
-                {prices?.map((price) => (
-                  <div key={price._id}>
-                    <Radio value={price.array}>{price.name}</Radio>
-                  </div>
-                ))}
+              {prices?.map((price) => (
+                <div key={price._id}>
+                  <Radio value={price.array}>{price.name}</Radio>
+                </div>
+              ))}
             </Radio.Group>
           </div>
           <button
             className="btn btn-primary"
-            onClick={() => { window.location.reload()}}>
-              Reset
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            Reset
           </button>
         </div>
         <div className="col-md-9">
           <div className="row g-1">
-            {products.length > 0
-              ? (products.map(product => 
-                  <div key={product._id} className="col-lg-3 col-md-4 col-6">
-                    <ProductCard key={product._id} product={product}/>
-                  </div>
-                )
-              ) : (<div>No product found</div>)
-            }
+            {products.length > 0 ? (
+              products.map((product) => (
+                <div key={product._id} className="col-lg-3 col-md-4 col-6">
+                  <ProductCard key={product._id} product={product} />
+                </div>
+              ))
+            ) : (
+              <div>No product found</div>
+            )}
           </div>
-          <Pagination 
+          <Pagination
             defaultCurrent={1}
             current={page}
             pageSize={limit}
-            total={total} 
-            onChange={(page)=> {setPage(page)}}
+            total={total}
+            onChange={(page) => {
+              setPage(page);
+            }}
           />
         </div>
       </div>
     </>
-  )
+  );
 }
